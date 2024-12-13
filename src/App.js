127380320e94b5f3d74f4c82a874d657f6fd4e31
Job Fernandez <jobandjeffy@gmail.com>
2024-12-13 23:08:19 +0530

    feat: :sparkles: home page basic ui

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "router/index.jsx";
+import "assets/css/fonts.css";
 export default function App() {
   return (
     <>
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,7 +1,16 @@
+import logoLight from "assets/images/logo/logoLight.svg";
 export default function HomePage() {
   return (
     <>
-      <p className="bg-red-400">Home Page</p>
+      <main className="bg-gradient-to-t from-[#B19EF3] from-10%  h-screen py-8 px-4 flex flex-col justify-between">
+        <img src={logoLight} alt="Logo" className="w-52 mx-auto" />
+        <button className="bg-white size-60 mx-auto block rounded-full shadow-lg text-red-500 text-5xl font-bold">
+          Quiz
+        </button>
+        <button className="w-full bg-red-500 py-3 text-2xl font-semibold rounded-3xl text-white">
+          Start
+        </button>
+      </main>
     </>
   );
 }
